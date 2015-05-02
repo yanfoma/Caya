eb929ff99ced194602f4bed7baa3902995badb55
Amon <amonmoce@yahoo.fr>
2015-05-02 20:04:19 +0800

    Web Interface ready -- Can choose the period and enter the numbers, the result on the page after hitting Submit

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'sinatra', :require => 'sinatra/base'
+gem 'haml'
--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'sinatra/base'
+require 'haml'
+require_relative 'check_number'
 
 class Caya < Sinatra::Base
+  include CheckNumber
 
   get '/' do
-    'Habari ! Caya is alive'
+    haml :index
   end
 
+  get '/check' do
+    @period = params[:period]
+    @number = params[:number]
+    if @period && @number
+      @result = win?(@period, @number)
+    end
+    haml :index
+  end
 
 end

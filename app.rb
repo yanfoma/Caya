baa386640a89dbffd7e06e88f876eaea205f31fb
Amon <amonmoce@yahoo.fr>
2015-05-15 14:49:20 +0800

    Added some post requests -- Added the multi_check feature in app.rb -- single_check and multi_check views prepared

--- app.rb
+++ app.rb
@@ -14,18 +14,31 @@ class Caya < Sinatra::Base
     haml :single_check
   end
 
-  get '/multi_check' do
-    haml :multi_check
-  end
-
-  get '/check' do
+  post '/single_check' do
     @period = params[:period]
     @number = params[:number]
-    if @period && @number
+    if @period && !@number.empty?
       @result = win?(@period, @number)
     end
     haml :single_check
   end
+
+  get '/multi_check' do
+    haml :multi_check
+  end
+
+  post '/multi_check' do
+    @period = params[:period]
+    @numbers = params[:numbers].gsub(/\s+/, "").split(',')
+    if @period && @numbers != []
+      @results = all_win?(@period, @numbers)
+    end
+    if @results == {}
+      @result = 'I am sorry, you have no prize'
+    end
+    haml :multi_check
+  end
+
 
   not_found do
     'Oops you made a mistake ... right ?'
